f5350296bf0bea286db19fcb0dd480f8303824a7
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-11-01 07:17:29 +0700

    feat: load .env configuration

--- src/main/java/cooklyst/utils/Env.java
+++ src/main/java/cooklyst/utils/Env.java
@@ -1,10 +1,14 @@
 package cooklyst.utils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Env {
-    public static final String DB_HOST = "localhost";
-    public static final String DB_PORT = "3306";
-    public static final String DB_NAME = "tes-wbd";
-    public static final String DB_USER = "root";
-    public static final String DB_PASS = "";
-    public static final String SOAP_KEY = "123";
+    private static final Dotenv dotenv = Dotenv.load();
+    public static final String DB_HOST = dotenv.get("MYSQL_HOST");
+    public static final String DB_PORT = dotenv.get("MYSQL_PORT");
+    public static final String DB_NAME = dotenv.get("MYSQL_DATABASE");
+    public static final String DB_USER = dotenv.get("MYSQL_USER", "root");
+    public static final String DB_PASS = DB_USER.equals("root") ? dotenv.get("MYSQL_ROOT_PASSWORD") : dotenv.get("MYSQL_PASSWORD");
+    public static final String APP_KEY = dotenv.get("APP_KEY");
+    public static final String REST_KEY = dotenv.get("REST_KEY");
 }
