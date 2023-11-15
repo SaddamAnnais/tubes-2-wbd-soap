b6b4e8a36328b8eac8a449e8d3e68f0f4dc59329
SaddamAnnais <saddamannaiss@gmail.com>
2023-11-15 13:15:38 +0700

    feat: domain filter and cors

---
+++ src/main/java/cooklyst/filter/DomainFilter.java
@@ -0,0 +1,28 @@
+package cooklyst.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class DomainFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String domain = httpRequest.getServerName();
+
+        if (domain.equals("localhost")) {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // do nothing
+    }
+}
